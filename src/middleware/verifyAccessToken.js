b59c55d5f14734af59b159a1af5ccf97b751ae62
Awez Mirza <awezbaigmirza@gmail.com>
2024-12-07 00:22:15 +0530

    Updated devError and prodError

--- src/middleware/verifyAccessToken.js
+++ src/middleware/verifyAccessToken.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { asyncEventHandler, customError } from "../errors/errorUtils/index.js";
 import { USER_SERVICE_URL } from "../config/index.js";
 
-const verifyAccessToken = asyncEventHandler(async (req, res, next) => {
+const verifyAccessToken = asyncEventHandler(async (req, _, next) => {
     const accessToken = req.headers["access-token"];
     if (!accessToken) {
         throw new customError(401, "Token is required");
